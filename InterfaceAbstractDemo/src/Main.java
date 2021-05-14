84a173bf5aa649803e674d8b1dd9832237dc57a6
OZ_TE <OZ_TE@192.168.1.29>
2021-05-15 00:34:25 +0300

    lesson 4 - homework 2

--- InterfaceAbstractDemo/src/Main.java
+++ InterfaceAbstractDemo/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
 		Customer customer1 = new Customer(1, "Engin", "Demiroð", 1985, "28861499108");
 		
-		Customer customer2 = new Customer(1, "Özkan", "Teke", 1993, "61141306024");
+		Customer customer2 = new Customer(1, "Özkan", "Teke", 1993, "61141789624");
 
 		customerManager.save(customer1);
 		customerManager.save(customer2);
