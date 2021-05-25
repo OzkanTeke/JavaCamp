ff48f619cc807af46c73150ffaf400d821a16d91
Özkan Teke <82838253+OzkanTeke@users.noreply.github.com>
2021-05-25 22:41:42 +0300

    lesson 7 - homework 1

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobsController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.JobService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.Job;
 
 @RestController
@@ -23,7 +27,12 @@ public class JobsController {
 	}
 	
 	@GetMapping("/getall")
-	public List<Job> getAll(){
+	public DataResult<List<Job>> getAll(){
 		return this.jobService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Job job) {
+		return this.jobService.add(job);
 	}
 }
--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Job.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Job.java
@@ -3,32 +3,27 @@ package kodlamaio.hrms.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "job_description")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Job {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "job_id")
 	private int id;
 	
 	@Column(name = "position")
 	private String position;
 	
-	public Job() {
-		
-	}
-
-	public Job(int id, String position) {
-		super();
-		this.id = id;
-		this.position = position;
-	}
-
 }
